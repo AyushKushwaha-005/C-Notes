4985c1b9b42bf9ef2e788365e9da2abe453a7986
Ayush Kushwaha <ayushtesla1111@gmail.com>
2026-04-16 23:26:43 +0530

    Add files via upload

---
+++ 5_airthmeticoperators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int x; //declaration
+    x = 5;
+    int y = 6;
+    
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+    
+ 
+ 
+}
