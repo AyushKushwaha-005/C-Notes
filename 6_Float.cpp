5e729709b7d0558818d61eb7456b2f97e3ad1839
Ayush Kushwaha <ayushtesla1111@gmail.com>
2026-04-17 20:08:43 +0530

    Add files via upload

---
+++ 6_Float.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x = 5.545;
+    float y = 6.5454;
+    
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+    
+ 
+ 
+}
---
+++ 7_incrementdecrementoperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    
+    cout<<x<<endl;
+    cout<<x++<<endl; //POST INCREMENT
+    cout<<x<<endl;
+
+    cout<<endl;
+    cout<<x<<endl;
+    cout<<++x<<endl; //PRE INCREMENT
+    cout<<x<<endl;
+    
+ 
+ 
+}
