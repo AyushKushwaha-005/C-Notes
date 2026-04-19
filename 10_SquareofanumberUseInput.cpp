d42b7b9e9de72aff61c298f3cb7c59c968d3b377
Ayush Kushwaha <ayushtesla1111@gmail.com>
2026-04-19 09:50:48 +0530

    Add files via upload

---
+++ 10_SquareofanumberUseInput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r ;
+    
+    cout<<"Enter The Number"<<endl;
+    cin>>r;
+    int squareis = r*r;
+    cout<<"The Square Of the Number IS";
+    cout<<squareis;
+}
