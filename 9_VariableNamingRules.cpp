266c127384f40db7d2c03aeb46f3afd8fb159c68
Ayush Kushwaha <ayushtesla1111@gmail.com>
2026-04-19 09:50:08 +0530

    Add files via upload

---
+++ 9_VariableNamingRules.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int _t =2;
+    cout<<_t<<endl;
+    // a b c d  H A S ALPHABETS
+    // 1224143 NUMBER
+    // @%^!%^#@!^%& {}[[ SPECIAL CARACTER
+    // $_ ARE ALLOWED FOR VARIABLE NAMING
+     
+}
