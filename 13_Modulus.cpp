ac1186b6f712f47a41d7ca979df07d7ad7894e4c
Ayush Kushwaha <ayushtesla1111@gmail.com>
2026-04-21 18:38:12 +0530

    Add files via upload

---
+++ 13_Modulus.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x = 8;
+    int y = 3;
+    cout<<x%y; //remainder
+}
+
+// if a%b = a         if a<b
+// a%a = 0
+// a%(-b)= a% b
+// (-a)%b = -[a%b]
