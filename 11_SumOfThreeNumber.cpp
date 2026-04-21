4b339a7555a81ab93e31fd7622a1fc75f26d549a
Ayush Kushwaha <ayushtesla1111@gmail.com>
2026-04-21 18:37:33 +0530

    Add files via upload

---
+++ 11_SumOfThreeNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Enter The First Number : ";
+    int x;
+    cin>>x;
+    cout<<"Enter The Second Number : ";
+    int y;
+    cin>>y;
+    cout<<"Enter The Third Number : ";
+    int z;
+    cin>>z;
+    cout<<"The Sum OF The Three Numbers Are : ";
+    int Sum = x+y+z;
+    cout<<Sum;
+
+
+}
